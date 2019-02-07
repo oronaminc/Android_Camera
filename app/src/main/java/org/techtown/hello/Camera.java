e6dc1ee2aeb09196ad2745b57fe9b7fd6eaa59d7
sunghyeon <sunghyeon86@gmail.com>
2019-02-07 22:24:12 +0900

    add camera authority

--- app/src/main/java/org/techtown/hello/Camera.java
+++ app/src/main/java/org/techtown/hello/Camera.java
@@ -1,21 +1,42 @@
 package org.techtown.hello;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
-        import android.os.Bundle;
-        import android.provider.MediaStore;
-        import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-        import android.widget.Button;
-        import android.widget.ImageView;
-        import android.widget.Toast;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class Camera extends AppCompatActivity implements View.OnClickListener {
+import java.io.File;
+
+public class Camera extends AppCompatActivity {
+
+    private static final int MY_PERMISSION_STORAGE = 1111;
+    private static final int REQUEST_TAKE_PHOTO = 2222;
+    private static final int REQUEST_TAKE_ALBUM = 3333;
 
     Button Button_Camera;
+    Button Button_Album;
     ImageView imageView;
+    File file;
 
+    Uri imageUri;
 
 
     @Override
@@ -23,29 +44,118 @@ public class Camera extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
+        String sdcard = Environment.getExternalStorageDirectory()+"/AP/";
+        //File sdcard = Environment.getExternalStorageDirectory();
+        File dir = new File(sdcard);
+
+        if(!dir.exists()){dir.mkdirs();}
+
+        file = new File(sdcard, "photo.jpg");
+
+        Toast.makeText(getApplicationContext(), file.toString(), Toast.LENGTH_SHORT).show();
+
+        checkPermission();
+
         Button_Camera = (Button) findViewById(R.id.camera);
+        Button_Album = (Button) findViewById(R.id.album);
         imageView = (ImageView) findViewById(R.id.view);
-        Button_Camera.setOnClickListener(this);
+
+        Button_Camera.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                Uri uri = FileProvider.getUriForFile(getApplicationContext(), "com.bignerdranch.android.test.fileprovider", file);
+                i.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                startActivityForResult(i,REQUEST_TAKE_PHOTO);
+            }
+        });
+
+        Button_Album.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent, REQUEST_TAKE_ALBUM);
+            }
+        });
+
+
     }
 
-    @Override
-    public void onClick(View view){
-        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(i,0);
-    }
 
     //  사진을 찍고 사진을 가져와요, intent의 결과를 여기서 확인할 수 있음
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if(requestCode==1){
-            if(resultCode==RESULT_OK){
-                imageView.setImageURI(data.getData());
-            }
+        if(requestCode==REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK){
+            Toast.makeText(getApplicationContext(),"성공", Toast.LENGTH_SHORT).show();
+            //imageView.setImageURI(data.getData());
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize=8;
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(),options);
+            imageView.setImageBitmap(bitmap);
+                //Bundle extras = data.getExtras();
+                //Bitmap imageBitmap = (Bitmap) extras.get("data");
+                //ImageView.setImageBitmap(imageBitmap);
         }else{
+            Toast.makeText(getApplicationContext(),"실패", Toast.LENGTH_SHORT).show();
             return ;
         }
         //data.getData()를 통해 방금 찍은 사진의 uri 가지고 일 수 있음
 
+    }
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // 다시 보지 않기 버튼을 만드려면 이 부분에 바로 요청을 하도록 하면 됨 (아래 else{..} 부분 제거)
+            // ActivityCompat.requestPermissions((Activity)mContext, new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSION_CAMERA);
+
+            // 처음 호출시엔 if()안의 부분은 false로 리턴 됨 -> else{..}의 요청으로 넘어감
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("알림")
+                        .setMessage("저장소 권한이 거부되었습니다. 사용을 원하시면 설정에서 해당 권한을 직접 허용하셔야 합니다.")
+                        .setNeutralButton("설정", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                intent.setData(Uri.parse("package:" + getPackageName()));
+                                startActivity(intent);
+                            }
+                        })
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();
+                            }
+                        })
+                        .setCancelable(false)
+                        .create()
+                        .show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSION_STORAGE);
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_STORAGE:
+                for (int i = 0; i < grantResults.length; i++) {
+                    // grantResults[] : 허용된 권한은 0, 거부한 권한은 -1
+                    if (grantResults[i] < 0) {
+                        Toast.makeText(getApplicationContext(), "해당 권한을 활성화 하셔야 합니다.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+                // 허용했다면 이 부분에서..
+
+                break;
+        }
     }
 
 }
